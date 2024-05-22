6f00313e84fc70cc2992d65e1b00ed4547b6819c
ABHISHEK KUMAR SINGH <abhishek.kumarsingh@hindujatech.com>
2024-05-22 15:25:03 +0530

    Update 330

--- book-a-car/src/main/java/com/example/bookacar/testcases/Checkbox.java
+++ book-a-car/src/main/java/com/example/bookacar/testcases/Checkbox.java
@@ -37,9 +37,30 @@ public class Checkbox {
 
     }
 	
-
-
-
+	@Test (priority=1)
+	
+	public void testCheckboxAndSpanElement() throws InterruptedException {
+        WebElement checkboxDiv = driver.findElement(By.xpath("//div[@class='ui-chkbox-box ui-widget ui-corner-all ui-state-default']"));
+        checkboxDiv.click();
+        WebElement checkboxIcon = checkboxDiv.findElement(By.xpath("//span[@class='ui-chkbox-icon ui-icon ui-c ui-icon-check']"));
+        boolean isChecked = checkboxIcon.isDisplayed();
+        Assert.assertTrue(isChecked, "Checkbox is not checked");
+    }
+	
+	
+    @Test (priority=2)
+    
+    public void Notification() {
+        WebElement checkboxDiv = driver.findElement(By.xpath("//div[@id='j_idt87:j_idt91']//div[contains(@class,'ui-chkbox-box')]"));
+        checkboxDiv.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement toastMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='j_idt87:msg_container']//div[contains(@class,'ui-growl-item-container')]")));
+        WebElement toastTitle = toastMessage.findElement(By.xpath(".//span[@class='ui-growl-title']"));
+        String toastText = toastTitle.getText();
+        Assert.assertEquals(toastText, "Checked", "The toast message does not contain the expected text 'Checked'");
+    }
+	    
+	
 	@AfterClass
 	   public void driverClose()
 	  {
